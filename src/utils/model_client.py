b34270a88f3562ddbf1797672357cab126ed027d
rajesh-nitc <rajesh.nitc@gmail.com>
2025-04-09 10:29:36 +0000

    nit: nit

--- src/utils/model_client.py
+++ src/utils/model_client.py
@@ -1,7 +1,5 @@
 from autogen_ext.models.openai import AzureOpenAIChatCompletionClient
 from langchain_openai import AzureOpenAIEmbeddings
-from langchain_postgres import PGVector
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.settings import settings
 
@@ -26,14 +24,4 @@ def get_azure_openai_embeddings_model_client() -> AzureOpenAIEmbeddings:
         api_key=settings.AZURE_OPENAI.API_KEY.get_secret_value(),
         azure_endpoint=settings.AZURE_OPENAI.ENDPOINT,
         api_version=settings.AZURE_OPENAI.API_VERSION,
-    )
-
-
-def get_pgvector_client(embeddings: AzureOpenAIEmbeddings, collection_name: str, db_session: AsyncSession) -> PGVector:
-    """Initialize the PGVector vector store."""
-    return PGVector(
-        embeddings=embeddings,
-        collection_name=collection_name,
-        connection=db_session,
-        use_jsonb=True,
     )
